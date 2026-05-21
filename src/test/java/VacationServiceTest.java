026b778eeb34e53016ab13e23fdbbdabafb8eef2
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-05-21 18:07:57 +0300

    Update VacationServiceTest.java

--- src/test/java/VacationServiceTest.java
+++ src/test/java/VacationServiceTest.java
@@ -4,41 +4,12 @@ public class VacationServiceTest {
     @Test
     public void testFirstExample() {
         VacationService service = new VacationService();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-
-        int expected = 3;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(3, service.calculate(10_000, 3_000, 20_000));
     }
 
     @Test
     public void testSecondExample() {
         VacationService service = new VacationService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3",
-            "100000, 60000, 150000, 2"
-    })
-    public void testMultipleScenarios(int income, int expenses, int threshold, int expected) {
-        VacationService service = new VacationService();
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(2, service.calculate(100_000, 60_000, 150_000));
     }
 }
