64335c7b04b9e63dde682ba06a68c6f76e30f7d6
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-05-21 18:07:25 +0300

    Update VacationService.java

--- src/main/java/org/example/VacationService.java
+++ src/main/java/org/example/VacationService.java
@@ -2,16 +2,16 @@ package org.example;
 
 public class VacationService {
     public int calculate(int income, int expenses, int threshold) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
+        int count = 0;
+        int money = 0;
 
         for (int month = 0; month < 12; month++) {
-            if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses; // тратим обязательные расходы
-                money = money / 3; // остаток уменьшаем в 3 раза (отдых)
+            if (money >= threshold) {
+                count++;
+                money = money - expenses;
+                money = money / 3; // остаток после отдыха (1/3 от суммы после expenses)
             } else {
-                money = money + income - expenses; // работаем: получаем доход и тратим расходы
+                money = money + income - expenses;
             }
         }
         return count;
